dbe18ff61fb167d6419b1132df7fad154b28bcc8
ThomasG1993 <570736711@qq.com>
2017-03-29 11:27:43 +0800

    bolt

--- src/main/java/com/asainfo/storm/StormBolt.java
+++ src/main/java/com/asainfo/storm/StormBolt.java
@@ -1,5 +1,27 @@
 package com.asainfo.storm;
 
-public class StormBolt {
+import java.util.Map;
 
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+
+public class StormBolt extends BaseRichBolt{
+
+	@Override
+	public void execute(Tuple arg0) {
+		
+	}
+
+	@Override
+	public void prepare(Map arg0, TopologyContext arg1, OutputCollector arg2) {
+		
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer arg0) {
+		
+	}
 }
